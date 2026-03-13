ce1a991c640039d1e6e778e909e5c8550b637f6c
nozadzelile <nozadzelile1@gmail.com>
2026-03-14 00:13:08 +0100

    search functionality using Api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
-
+    let cityElement = document.querySelector("#city");
+    
+    cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
 }
 
@@ -14,8 +16,7 @@ function searchCity(city) {
 function handaleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    
     searchCity(searchInput.value);
 }
 
