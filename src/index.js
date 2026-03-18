bbc1d40ce820256abff155ef787ca1f76def24c7
nozadzelile <nozadzelile1@gmail.com>
2026-03-18 17:13:09 +0100

    added API on forecast,date and time.

--- src/index.js
+++ src/index.js
@@ -56,25 +56,39 @@ function handaleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
-function displayForecast() {
-    let days = ["Wed", "Thu", "Fri", "Sat", "Sun"];
+function dailyForecast(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "bc0c2310f91t07094a4ao08773084492";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
     let forecastHtml = "";
 
-    days.forEach(function (day) {
+    response.data.daily.forEach(function (day, index) {
+        if (index > 0 && index < 6) {
         forecastHtml =
-        forecastHtml +
-        `<div class="weather-forecast-day">
-        <div class="forecast-date">${day}</div>
-        <div class="forecast-icon">🌤️</div>
-        <div class="forecast-temperatures">
+         forecastHtml +
+         `<div class="weather-forecast-day">
+         <div class="forecast-date">${dailyForecast(day.time)}</div>
+
+         <img src="${day.condition.icon_url}" class="forecast-icon" />
+         <div class="forecast-temperatures">
           <div class="forecast-temperature">
-            <strong>15º</strong>
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
           </div>
-          <div class="forecast-temperature">9º</div>
+          <div class="forecast-temperature">${Math.round(day.temperature.minimum)}º</div>
         </div>
       </div>`;
+        }
 });
-
 
 let forecastElement = document.querySelector("#forecast");
 forecastElement.innerHTML = forecastHtml;
@@ -85,4 +99,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handaleSearchSubmit);
 
 searchCity("Barcelona");
-displayForecast();
